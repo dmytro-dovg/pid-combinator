f01f44f787b1e973c516b7b338e543665d31e54b
Dmytro Dovgoshliubnyi <dmytro@dovgo.sh>
2026-06-29 22:26:13 +0100

    feat: added PID combinator remnants

--- prototypes/pid-combinator-remnants.lua
+++ prototypes/pid-combinator-remnants.lua
@@ -2,5 +2,6 @@
 local remnants = table.deepcopy(data.raw["corpse"]["arithmetic-combinator-remnants"])
 remnants.name = "pid-combinator-remnants"
 remnants.icon = "__pid-combinator__/graphics/icons/pid-combinator.png"
+remnants.animation.filename = "__pid-combinator__/graphics/entity/combinator/remnants/pid/pid-combinator-remnants.png"
 
 data:extend({ remnants })
